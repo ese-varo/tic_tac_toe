7cfc64876e1b37fd26aaace1c04690eddbfb86bb
Alvaro <varoboss.ap@gmail.com>
2020-01-20 21:58:01 -0600

    Create print_matrix board's method

--- board.rb
+++ board.rb
@@ -16,19 +16,45 @@ class Board
   end
 
   def fill_matrix
-    c = 1
     size.times do |i|
       matrix.push([])
       size.times do |j|
-        cell = Cell.new(c, "%03d"%c)
-        matrix[i][j] = cell 
-        c += 1
+        c = i * size + j + 1
+        matrix[i][j] = Cell.new(c, "%03d"%c)
       end
     end
+  end
+
+  def print_matrix
+    size.times do |i|
+      top_and_bottom_lines(size * 8 + 1)
+      before_and_after_lines
+      numbers_line(i)
+      before_and_after_lines
+    end
+    top_and_bottom_lines(size * 8 + 1)
   end
 
   private 
 
   attr_writer :matrix
+
+  def top_and_bottom_lines(n)
+    line = ""
+    n.times { line += "=" }
+    puts line
+  end
+
+  def before_and_after_lines
+    line = ""
+    size.times { line += "|       "}
+    puts line += "|"
+  end
+
+  def numbers_line(index)
+    line_of_numbers = ""
+    size.times { |j| line_of_numbers += "|  #{matrix[index][j].label}  " }
+    puts line_of_numbers += "|"
+  end
 
 end
